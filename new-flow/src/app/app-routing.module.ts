256e14d14ba7647b9805c4b7fb618be1f6d3d59b
RayanBlb <78950777+RayanBlb@users.noreply.github.com>
2022-09-15 21:34:40 +0200

    ui without logic

--- new-flow/src/app/app-routing.module.ts
+++ new-flow/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { SuccessPageComponent } from './components/success-page/success-page.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'success', component: SuccessPageComponent },
+  { path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- new-flow/src/app/app.module.ts
+++ new-flow/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SuccessPageComponent } from './components/success-page/success-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SuccessPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
