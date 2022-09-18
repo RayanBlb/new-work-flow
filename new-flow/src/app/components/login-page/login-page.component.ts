f12ae0fc4c09114320b7ab81fab27f46084ef91c
RayanBlb <78950777+RayanBlb@users.noreply.github.com>
2022-09-19 00:57:36 +0200

    first test

--- new-flow/src/app/components/login-page/login-page.component.ts
+++ new-flow/src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'app-login-page',
@@ -10,6 +11,25 @@ export class LoginPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  authenticateLogin() {
+    const keycloak = new Keycloak({
+      url: 'http://localhost:8080',
+      realm: 'Angular-web',
+      clientId: 'angular-web-client'
+    });
+
+    keycloak.init({ onLoad: 'login-required', flow: 'standard', redirectUri: window.location.origin + "/success" }).then((auth) => {
+      if (!auth) {
+        window.location.reload();
+      } else {
+        console.log("Authenticated");
+      }
+      sessionStorage.setItem("authKey", keycloak.token!);
+    }).catch((error) => {
+      console.error("Authenticated failed due to \n" + error);
+    });
   }
 
 }
--- new-flow/src/app/components/success-page/success-page.component.ts
+++ new-flow/src/app/components/success-page/success-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'app-success-page',
@@ -10,6 +11,21 @@ export class SuccessPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+      // const keycloak = new Keycloak({
+      //   url: 'http://localhost:8080',
+      //   realm: 'Angular-web',
+      //   clientId: 'angular-web-client'
+      // });
+
+      // keycloak.init({ }).then((auth) => {
+      //   if (!auth) {
+      //     window.location.reload();
+      //   } else {
+      //     console.log("Authenticated");
+      //   }
+      // }).catch((error) => {
+      //   console.error("Authenticated failed due to \n" + error);
+      // });
   }
 
 }
